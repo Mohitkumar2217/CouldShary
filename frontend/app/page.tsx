4c33b4e346e51f4aa5c1e296360f91b3ba8e9e5a
Mohit Kumawat <mohitkumawat221004@gmail.com>
2026-07-30 02:54:23 +0530

    docker-compose setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-       
+    <div className="p-8">
+      <Button>Click me</Button>
+      <Button variant="outline">Secondary</Button>
+      <Button variant="destructive">Delete</Button>
     </div>
   );
 }
