6106e34c0a9a50f6c7302e8661bfddfbd3a9f025
Mohit Kumawat <mohitkumawat221004@gmail.com>
2026-08-01 20:08:52 +0530

    files upload tesing apis end points

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoutes); 
 app.use("/files", filesRoutes);
