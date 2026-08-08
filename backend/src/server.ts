fcaa109c18a74ba6a5069f301c9b23f4612c8466
Mohit Kumawat <mohitkumawat221004@gmail.com>
2026-08-08 18:32:04 +0530

    server port update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ import { scheduleReapeatableJobs } from "./queues/scheduler.js";
 
 import express from "express";
 import cors from "cors";
-import helmet from "helmet"; 
+import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.js";
 import { requireAuth, requireRole } from "./middleware/auth.js";
@@ -38,7 +38,7 @@ const app = express();
 
 // app.use("/admin/queues", serverAdapter.getRouter());
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.use(helmet());
 app.use(cors({
@@ -49,7 +49,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/auth", authRoutes); 
+app.use("/auth", authRoutes);
 app.use("/files", filesRoutes);
 app.use("/files", chunkedUploadRoutes);
 app.use("/folders", folderRoutes);
@@ -57,15 +57,18 @@ app.use("/", shareLinkRoutes);
 app.use("/", fileAccessRoutes);
 
 app.get("/me", requireAuth, (req, res) => {
-  res.json({user: req.user});
+  res.json({ user: req.user });
 });
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
 
-const PORT = process.env.PORT || 7000;
-app.listen(PORT, () => console.log(`API running on port ${PORT}`));
+const PORT = Number(process.env.PORT ?? 7000);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`API running on port ${PORT}`);
+});
 
 // after app. listen(...)
 scheduleReapeatableJobs();
